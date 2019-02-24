7cc5bbba67c07d9457c63b066c11b03a65a8c70c
Mohamed Kechaou <mohamed.kechaou@alumni.fh-aachen.de>
2019-02-24 13:32:30 +0100

    Change Animation Color

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 $(function(){
 	$(".typed").typed({
-		strings: ["UI/UX Designer", "Web Developper", "Student"],
+		strings: ["UI/UX Designer .", "Web Developper .", "Student ."],
 		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 		stringsElement: null,
 		// typing speed
